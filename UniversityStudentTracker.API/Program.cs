97e41f4bf53418356e5cff939fc5849696b02693
SanjulaD <desanjula99@gmail.com>
2024-07-01 23:31:28 +0530

    feat: remove unwanted

--- UniversityStudentTracker.API/Program.cs
+++ UniversityStudentTracker.API/Program.cs
@@ -27,10 +27,11 @@ builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
 
 var app = builder.Build();
 
+app.UseSwagger();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
     app.UseSwaggerUI();
 }
 
